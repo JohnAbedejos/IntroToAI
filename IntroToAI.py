68bf8a9366f1bda701aa8580cc6983ddf1a43772
mdpvs98 <mdpvs1998@gmail.com>
2020-12-16 16:31:48 +0000

    Added files and imported libraries

--- IntroToAI.py
+++ IntroToAI.py
@@ -5,4 +5,20 @@ Created on Mon Dec 14 17:15:17 2020
 @author: JohnA
 """
 
-#Testing push
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+from scipy import interp
+import math
+from scipy.stats import norm
+from scipy import stats
+
+#Don't know if newcessary
+import warnings
+warnings.filterwarnings('ignore') # Disabling warnings for clearer outputs.
+pd.options.display.max_columns = 50 # Pandas option to increase max number of columns to display.
+plt.style.use('ggplot') # Setting default plot style.
+

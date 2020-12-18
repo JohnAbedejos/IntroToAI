72efdf77f8bebb2dbd9c581ed8574c1a289b4f79
mdpvs98 <mdpvs1998@gmail.com>
2020-12-18 20:40:06 +0000

    Update IntroToAI.py

--- IntroToAI.py
+++ IntroToAI.py
@@ -31,9 +31,9 @@ from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import RFE
 
-#import xgboost as xgb
-#import lightgbm as lgb
-#from mlxtend.plotting import plot_decision_regions
+import xgboost as xgb
+import lightgbm as lgb
+from mlxtend.plotting import plot_decision_regions
 
 #Don't know if necessary
 import warnings
@@ -44,12 +44,12 @@ plt.style.use('ggplot') # Setting default plot style.
 #Import training file and check
 training_data = pd.read_csv('train.csv')
 index = len(training_data)
-display(training_data.sample(5))
+#display(training_data.sample(5))
 
 #Import test file and check
 testing_data = pd.read_csv('test.csv')
 index = len(testing_data)
-display(testing_data.sample(5))
+#display(testing_data.sample(5))
 
 #We decided it was best to merge both datasets for this piece of work and compare between them:
 
@@ -89,7 +89,7 @@ plotFrequency(categories)
 def plotsurvival(categories, data):
     
     '''A plot for bivariate analysis.'''
-    
+  
     fig, axes = plt.subplots(math.ceil(len(categories) / 3), 3, figsize=(20, 12))
     axes = axes.flatten()
 
@@ -135,71 +135,22 @@ def status(feature):
     
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
-'''
-#Modelling preparation
-def get_combined_data():
-    # Reading train data:
-    #train = pd.read_csv('train.csv')
-
-    # Reading test data:
-    #test = pd.read_csv('test.csv')
-
-    # Extracting the targets from the training data:
-    targets = train.Survived
-
-    # Merging train data and test data for future feature engineering:
-    combined = train.append(test)
-    combined.reset_index(inplace=True, drop=True)
-
-    return combined
 
 
-# Filling in the missing values for Age:
-def age_fill():
-    global combined
-    
-    combined['Age'] = combined.groupby(['Pclass', 'Sex'])['Age'].apply(lambda x: x.fillna(x.median()))
-    status('Age')
-    return combined
-
-def fare_fill():
-    global combined
-
-    # Filling missing values in fare:
-
-    combined['Fare'] = combined.groupby(
-        ['Pclass', 'Sex'])['Fare'].apply(lambda x: x.fillna(x.median()))
-    status('fare')
-    return combined
-
-combined = get_combined_data()  # For merging train test data.
-#combined = family_survival()  # For creating family survival feature.
-#combined = process_family()  # For creating Family size feature.
-#combined = get_titles()  # For extracting titles.
-#combined = process_names()  # For one hot encoding titles.
-combined = age_fill()  # For imputing missing age values.
-#combined = age_binner()  # For grouping and hot encoding age ranges.
-combined = fare_fill()  # For imputing fares.
-# For grouping and label encoding fares, can use 'both' for keeping age with dummies or yes for just one hot encoding.
-#combined = process_fare_bin(onehot='no')
-# combined =scale_fare() # For scaling age values.
-#combined = process_embarked()  # For imputing embarked and one hot encoding.
-# combined = process_cabin() # For extracting deck info from cabins.
-#combined = process_sex()  # For label encoding sex.
-# combined = process_pclass() # For one hot encoding pclass.
-# combined = process_ticket() # For extracting ticket info.
-#combined = dropper()  # For dropping not needed features.
-
-print(
-    f'Processed everything. Missing values left: {combined.isna().sum().sum()}'
-)
-'''
 def get_train_data():
 
     training = train
     training.reset_index(inplace=True, drop=True)
 
     return training
+
+def get_test_data():
+
+    testing = test
+    testing.reset_index(inplace=True, drop=True)
+
+    return testing
+
 
 # Filling in the missing values for Age:
 def age_fill():
@@ -209,20 +160,251 @@ def age_fill():
     status('Age')
     return training
 
-training = get_train_data()
-training = age_fill()
+# Filling in the missing values for Age:
+def age_fill2():
+    global testing
+    
+    testing['Age'] = testing.groupby(['Pclass', 'Sex'])['Age'].apply(lambda x: x.fillna(x.median()))
+    status('Age')
+    return testing
 
-fig, ax = plt.subplots(ncols=2, figsize=(20, 6))
+
+# Ranging and grouping Ages
+def age_range():
+    global training
+    
+    bins = [0, 3, 18, 40, 70, np.inf]
+    
+    names = ['Under_3', '3-18', '18-40', '40-70', 'Over_70']
+
+    training['Age Range'] = pd.cut(training['Age'], bins, labels=names)
+    
+    age_dummies = pd.get_dummies(training['Age Range'], prefix='Age Range')
+    training = pd.concat([training, age_dummies], axis=1)
+    training.drop('Age Range', inplace=True, axis=1)
+    training.drop('Age', inplace=True, axis=1)
+    status('Age Bins')
+    
+    return training
+
+def age_range2():
+    global testing
+    
+    bins = [0, 3, 18, 40, 70, np.inf]
+    
+    names = ['Under_3', '3-18', '18-40', '40-70', 'Over_70']
+
+    testing['Age Range'] = pd.cut(testing['Age'], bins, labels=names)
+    
+    age_dummies = pd.get_dummies(testing['Age Range'], prefix='Age Range')
+    testing = pd.concat([testing, age_dummies], axis=1)
+    testing.drop('Age Range', inplace=True, axis=1)
+    testing.drop('Age', inplace=True, axis=1)
+    status('Age Bins')
+    
+    return testing
+
+
+# Filling missing values in fare:
+def fare_fill():
+    global testing
+
+    testing['Fare'] = testing.groupby(['Pclass', 'Sex'])['Fare'].apply(lambda x: x.fillna(x.median()))
+    status('fare')
+    return testing
+
+
+# Filling missing embarked values with the most frequent one:
+def embarked_fill():
+    
+    global training
+    
+    training.Embarked.fillna(training.Embarked.mode()[0], inplace=True)
+    
+    # One hot encoding.
+    
+    embarked_dummies = pd.get_dummies(training['Embarked'], prefix='Embarked')
+    training = pd.concat([training, embarked_dummies], axis=1)
+    training.drop('Embarked', axis=1, inplace=True)
+    status('Embarked')
+    
+    return training
+
+
+def gender_mapping():
+    global training
+    
+    # Mapping string values with numerical ones.
+    
+    training['Sex'] = training['Sex'].map({'male': 0, 'female': 1})
+    status('Sex')
+    return training
+
+
+def gender_mapping2():
+    global testing
+    
+    # Mapping string values with numerical ones.
+    
+    testing['Sex'] = testing['Sex'].map({'male': 0, 'female': 1})
+    status('Sex')
+    return testing
+
+
+del train['Cabin']
+del test['Cabin']
+
+training = get_train_data()
+testing = get_test_data()
+training = age_fill()
+testing = age_fill2()
+training = age_range()
+testing = age_range2()
+testing = fare_fill()
+training = embarked_fill()
+training = gender_mapping()
+testing = gender_mapping2()
+
+
+fig, ax = plt.subplots(ncols=2, figsize=(30, 15))
+#fig.tight_layout()
 sns.heatmap(training_data.isnull(), yticklabels=False, cbar=False, cmap='binary', ax=ax[0])
 sns.heatmap(training.isnull(), yticklabels=False, cbar=False, cmap='binary', ax=ax[1])
  
-ax[0].set_title('Training Data Missing Values')
-ax[1].set_title('Testing Data Missing Values')
+ax[0].set_title('Training Data with missing Values')
+ax[1].set_title('Training Data after processing age')
 
 plt.xticks(rotation=90)
 plt.show()    
     
-    
-    
-    
-    
+fig, ax = plt.subplots(ncols=2, figsize=(30, 15))
+#fig.tight_layout()
+sns.heatmap(testing_data.isnull(), yticklabels=False, cbar=False, cmap='binary', ax=ax[0])
+sns.heatmap(testing.isnull(), yticklabels=False, cbar=False, cmap='binary', ax=ax[1])
+ 
+ax[0].set_title('Testing Data with missing Values')
+ax[1].set_title('Testing Data after processing age')
+
+plt.xticks(rotation=90)
+plt.show()
+
+#display(training.sample(10))
+training_copy = training.copy()
+del training_copy['PassengerId']
+
+sns.set(font_scale=1.1)
+correlation_train = training_copy.corr()
+mask = np.triu(correlation_train.corr())
+plt.figure(figsize=(18, 15))
+sns.heatmap(correlation_train,
+            annot=True,
+            fmt='.1f',
+            cmap='coolwarm',
+            square=True,
+            mask=mask,
+            linewidths=1)
+
+plt.show()
+#TEST
+def recover_train_test_target():
+    global training
+    y = pd.read_csv('train.csv', usecols=['Survived'])['Survived']
+    X = training.iloc[:index]
+    X_test = training.iloc[index:]
+
+    return X, X_test, y
+
+X, X_test, y = recover_train_test_target()
+
+print(f'X shape: {X.shape}')
+print(f'y shape: {y.shape}')
+print(f'X_test shape: {X_test.shape}')
+
+
+#Modelling Section
+cv = StratifiedKFold(10, shuffle=True, random_state=42)
+
+rf = RandomForestClassifier(criterion='gini',
+                            n_estimators=1750,
+                            max_depth=7,
+                            min_samples_split=6,
+                            min_samples_leaf=6,
+                            max_features='auto',
+                            oob_score=True,
+                            random_state=42,
+                            n_jobs=-1,
+                            verbose=0)
+
+lg = lgb.LGBMClassifier(max_bin=4,
+                        num_iterations=550,
+                        learning_rate=0.0114,
+                        max_depth=3,
+                        num_leaves=7,
+                        colsample_bytree=0.35,
+                        random_state=42,
+                        n_jobs=-1)
+
+xg = xgb.XGBClassifier(
+    n_estimators=2800,
+    min_child_weight=0.1,
+    learning_rate=0.002,
+    max_depth=2,
+    subsample=0.47,
+    colsample_bytree=0.35,
+    gamma=0.4,
+    reg_lambda=0.4,
+    random_state=42,
+    n_jobs=-1,
+)
+
+sv = SVC(probability=True)
+
+logreg = LogisticRegression(n_jobs=-1, solver='newton-cg')
+
+gb = GradientBoostingClassifier(random_state=42)
+
+gnb = GaussianNB()
+
+mlp = MLPClassifier(random_state=42)
+
+estimators = [rf, lg, xg, gb, sv, logreg, gnb, mlp]
+
+
+def model_check(X, y, estimators, cv):
+    model_table = pd.DataFrame()
+
+    row_index = 0
+    for est in estimators:
+
+        MLA_name = est.__class__.__name__
+        model_table.loc[row_index, 'Model Name'] = MLA_name
+        #    model_table.loc[row_index, 'MLA Parameters'] = str(est.get_params())
+
+        cv_results = cross_validate(
+            est,
+            X,
+            y,
+            cv=cv,
+            scoring='accuracy',
+            return_train_score=True,
+            n_jobs=-1
+        )
+
+        model_table.loc[row_index, 'Train Accuracy Mean'] = cv_results[
+            'train_score'].mean()
+        model_table.loc[row_index, 'Test Accuracy Mean'] = cv_results[
+            'test_score'].mean()
+        model_table.loc[row_index, 'Test Std'] = cv_results['test_score'].std()
+        model_table.loc[row_index, 'Time'] = cv_results['fit_time'].mean()
+
+        row_index += 1
+
+    model_table.sort_values(by=['Test Accuracy Mean'],
+                            ascending=False,
+                            inplace=True)
+
+    return model_table
+
+raw_models = model_check(X, y, estimators, cv)
+display(raw_models.style.background_gradient(cmap='summer_r'))
+
